6765e7dee867941b6c8f37b563fbcb1d3fe6467e
Kevin Nelson <km.nelson@outlook.com>
2024-08-05 12:59:41 -0400

    added tests to validate that the form correctly display the input typed into them

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -13,5 +13,10 @@ describe('URL Shortener Page', () => {
     })
   })
 
-  
+  it('when user fills out the form, the information is reflected in the input fields', () => {
+    cy.get('input[name="title"]').type('Dog photo')
+    cy.get('input[name="title"]').should('have.value', 'Dog photo')
+    cy.get('input[name="long_url"]').type('https://images.unsplash.com/photo-1531898418865-480b7090470f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80')
+    cy.get('input[name="long_url"]').should('have.value', 'https://images.unsplash.com/photo-1531898418865-480b7090470f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80')
+  })
 })

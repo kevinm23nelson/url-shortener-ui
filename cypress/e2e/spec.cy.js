bdd07e61a7d42f8c760bd0931851faed3743747a
Kevin Nelson <km.nelson@outlook.com>
2024-08-05 12:52:07 -0400

    added testing to validate everything on the page is correct upon load and fetch is intercepted and stubbed

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,17 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('URL Shortener Page', () => {
+  beforeEach(() => {
+    cy.setupIntercepts()
+    cy.visit("http://localhost:3000")
   })
+
+  it('should display the correct title, required form elements, and existing url from BE(fixture)', () => {
+    cy.get('.App').should('contain.text', "URL Shortener")
+    cy.get('form').within(() => {
+      cy.get('input[name="title"]').should('have.attr', 'placeholder', 'Title...')
+      cy.get('input[name="long_url"]').should('have.attr', 'placeholder', 'URL to Shorten...')
+      cy.get('button').should('contain.text', 'Shorten Please!')
+    })
+  })
+
+  
 })

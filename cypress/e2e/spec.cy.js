edaba208ee89c648037e13aa769e2287e27c9db3
Kevin Nelson <km.nelson@outlook.com>
2024-08-05 13:24:05 -0400

    finished all iterations with testing - reviewing

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -4,7 +4,7 @@ describe('URL Shortener Page', () => {
     cy.visit("http://localhost:3000")
   })
 
-  it('should display the correct title, required form elements, and existing url from BE(fixture)', () => {
+  it('should display the correct title and required form elements', () => {
     cy.get('.App').should('contain.text', "URL Shortener")
     cy.get('form').within(() => {
       cy.get('input[name="title"]').should('have.attr', 'placeholder', 'Title...')
@@ -13,10 +13,29 @@ describe('URL Shortener Page', () => {
     })
   })
 
+  it('should fetch the data from fixture', () => {
+    cy.get('.urls-container')
+      .get('.urls-card').should('have.length', 2)
+      .get('.urls-card').first().should('contain.text', 'Awesome photo')
+      .get('.urls-card').last().should('contain.text', 'Dog photo')
+  })
+
   it('when user fills out the form, the information is reflected in the input fields', () => {
     cy.get('input[name="title"]').type('Dog photo')
     cy.get('input[name="title"]').should('have.value', 'Dog photo')
     cy.get('input[name="long_url"]').type('https://images.unsplash.com/photo-1531898418865-480b7090470f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80')
     cy.get('input[name="long_url"]').should('have.value', 'https://images.unsplash.com/photo-1531898418865-480b7090470f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80')
+  })
+
+  it('user can fill out and submit the form and post the new ur and intercept post from fixture', () => {
+    cy.get('input[name="title"]').type('Cat photo')
+    cy.get('input[name="long_url"]').type('https://images.unsplash.com/photo-1531898418865-480b7090470f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80')
+    cy.get('button').should('contain.text', 'Shorten Please!').click()
+
+    cy.wait('@postURL')
+    cy.get('.urls-container')
+      .get('.urls-card').should('have.length', 3)
+      .get('.urls-card').first().should('contain.text', 'Awesome photo')
+      .get('.urls-card').last().should('contain.text', 'Cat photo')
   })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,6 +1,11 @@
 Cypress.Commands.add('setupIntercepts', () => {
-    cy.intercept('GET', 'http://localhost:3001/api/v1/urls' , {
+    cy.intercept('GET', 'http://localhost:3001/api/v1/urls', {
         statusCode: 200,
         fixture: 'urls.json'
     }).as('getUrls')
+
+    cy.intercept('POST', 'http://localhost:3001/api/v1/urls', {
+        statusCode: 201,
+        fixture: 'post.json'
+    }).as('postURL')
 })
